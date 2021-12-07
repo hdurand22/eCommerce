e1d641ad0fd6ab11c25b573f025c54a03d733653
hdurand22 <hdurand22@gmail.com>
2021-12-07 15:32:24 -0800

    remove from cart functionality

--- frontend/src/views/CartScreen.js
+++ frontend/src/views/CartScreen.js
@@ -3,7 +3,7 @@ import { Link, useParams, useSearchParams, useNavigate } from 'react-router-dom'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 
 const CartScreen = () => {
@@ -25,7 +25,7 @@ const CartScreen = () => {
     }, [dispatch, productId, qty])
 
     const removeFromCartHandler = (id) => {
-        console.log('remove');
+        dispatch(removeFromCart(id))
     }
 
     const checkoutHandler = () => {
